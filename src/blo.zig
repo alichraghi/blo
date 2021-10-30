7aa2d78477052db792564ac8e542621df893a289
alichraghi <chraghiali1@gmail.com>
2021-10-30 22:02:57 +0330

    everything works

---
+++ src/blo.zig
@@ -0,0 +1,209 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("pwd.h");
+});
+const mem = std.mem;
+const io = std.io;
+const math = std.math;
+const fs = std.fs;
+const os = std.os;
+
+pub const Blo = struct {
+    const Self = Blo;
+    const max_file_size = 1024 * 1024 * 10;
+
+    // zig fmt: off
+    pub const Config = struct {
+        ascii_chars: bool,
+        info: bool,
+        colors: bool,
+        show_end: bool,
+        line_number: bool
+    };
+    // zig fmt: on
+
+    allocator: *mem.Allocator,
+    out: fs.File,
+    writer: fs.File.Writer,
+    config: Config,
+
+    pub fn init(allocator: *mem.Allocator, out: fs.File, config: Config) Self {
+        return .{ .allocator = allocator, .out = out, .writer = out.writer(), .config = config };
+    }
+
+    fn digitLen(n: usize) usize {
+        if (n < 10) return 1;
+        return 1 + digitLen(n / 10);
+    }
+
+    pub fn write(self: Self, data: []const u8) !void {
+        try self.writer.writeAll(data);
+    }
+
+    pub fn writeNTime(self: Self, data: []const u8) !void {
+        try self.writer.writeByteNTimes(data);
+    }
+
+    fn writeSliceNTime(self: Self, slice: []const u8, n: usize) !void {
+        var i = n;
+        while (i > 0) : (i -= 1) {
+            try self.writer.writeAll(slice);
+        }
+    }
+
+    fn print(self: Self, comptime format: []const u8, args: anytype) !void {
+        try self.writer.print(format, args);
+    }
+
+    // Format to human readable size like 10B, 10KB, etc
+    fn fmtSize(self: Self, bytes: usize) ![]const u8 {
+        const suffix = &[_][]const u8{ "B", "KB", "MB" };
+        var size = bytes;
+        var i: usize = 0;
+        while (size >= 1024) {
+            size /= 1024;
+            i += 1;
+        }
+        return std.fmt.allocPrint(self.allocator, "{d} {s}", .{ size, suffix[i] }) catch "Unkown";
+    }
+
+    // TODO: this function is unsafe (out of bound index)
+    fn fillSlice(slice: []u8, value: []const u8) void {
+        var i: usize = 0;
+        while (i < slice.len) : (i += value.len) {
+            mem.copy(u8, slice[i..][0..value.len], value);
+        }
+    }
+
+    const Color = enum { Reset, Black, Red, Green, Yellow, Blue, Magenta, Cyan, White, Gray, BrightRed, BrightGreen, BrightYellow, BrightBlue, BrightMagenta, BrightCyan, BrightWhite };
+
+    fn setColor(self: Self, value: []const u8, color: Color) ![]const u8 {
+        if (self.config.colors) {
+            var res = std.ArrayList(u8).init(self.allocator);
+            defer res.deinit();
+            try res.appendSlice(self.getColor(color));
+            try res.appendSlice(value);
+            try res.appendSlice(self.getColor(.Reset));
+            return res.toOwnedSlice();
+        } else return value;
+    }
+
+    fn getColor(self: Self, color: Color) []const u8 {
+        return if (self.config.colors)
+            switch (color) {
+                .Reset => "\x1b[0m",
+                .Black => "\x1b[30m",
+                .Red => "\x1b[31m",
+                .Green => "\x1b[32m",
+                .Yellow => "\x1b[33m",
+                .Blue => "\x1b[34m",
+                .Magenta => "\x1b[35m",
+                .Cyan => "\x1b[36m",
+                .White => "\x1b[37m",
+                .Gray => "\x1b[90m",
+                .BrightRed => "\x1b[91m",
+                .BrightGreen => "\x1b[92m",
+                .BrightYellow => "\x1b[93m",
+                .BrightBlue => "\x1b[94m",
+                .BrightMagenta => "\x1b[95m",
+                .BrightCyan => "\x1b[96m",
+                .BrightWhite => "\x1b[97m",
+            }
+        else
+            &[_]u8{};
+    }
+
+    pub fn printFile(self: Self, path: []const u8) !void {
+        const file = try fs.cwd().openFile(path, .{});
+        const data = try file.readToEndAlloc(self.allocator, max_file_size);
+
+        if (self.config.info) {
+            // file stat
+            // Posix Only, no Windows support :)
+            const stat = try std.os.fstat(file.handle);
+
+            // file size
+            const size = try self.fmtSize(@intCast(usize, stat.size));
+
+            // file owner name
+            const passwd = extern struct { pw_name: [*:0]u8 };
+            const owner: []const u8 = mem.sliceTo(@ptrCast(*passwd, c.getpwuid(stat.uid) orelse {
+                return error.InvalidUID;
+            }).pw_name, 0);
+
+            // file info
+            const stat_len = 3; // path - size - owner
+            const total_stat_len = path.len + size.len + owner.len;
+            const space = if (self.config.line_number) " " ** 5 else "";
+
+            // printing
+            if (self.config.ascii_chars) {
+                const width_len = (total_stat_len + (stat_len * 3) + 5);
+                const width = try self.allocator.alloc(u8, width_len);
+                defer self.allocator.free(width);
+                for (width) |*char| char.* = '=';
+
+                try self.print("{s}{s}\n{s}== {s} == {s} == {s} ==\n{s}{s}\n", .{ space, width, space, path, size, owner, space, width });
+            } else {
+                const side_margin = 2;
+                const brick = "─";
+                var path_width = try self.allocator.alloc(u8, (path.len + side_margin) * brick.len);
+                var size_width = try self.allocator.alloc(u8, (size.len + side_margin) * brick.len);
+                var owner_width = try self.allocator.alloc(u8, (owner.len + side_margin) * brick.len);
+                fillSlice(path_width, brick);
+                fillSlice(size_width, brick);
+                fillSlice(owner_width, brick);
+                defer {
+                    self.allocator.free(path_width);
+                    self.allocator.free(size_width);
+                    self.allocator.free(owner_width);
+                }
+
+                const left_bottom_corner = if (self.config.line_number) "├" else "└";
+
+                try self.print("{s}{s}┌{s}┬{s}┬{s}┐\n{s}│{s} {s} {s}│{s} {s} {s}│{s} {s} {s}│\n{s}{s}{s}┴{s}┴{s}┘{s}\n", .{
+                    space,
+                    self.getColor(.Gray),
+                    path_width,
+                    size_width,
+                    owner_width,
+                    space,
+                    self.getColor(.Yellow),
+                    path,
+                    self.getColor(.Gray),
+                    self.getColor(.Magenta),
+                    size,
+                    self.getColor(.Gray),
+                    self.getColor(.Green),
+                    owner,
+                    self.getColor(.Gray),
+                    space,
+                    left_bottom_corner,
+                    path_width,
+                    size_width,
+                    owner_width,
+                    self.getColor(.Reset),
+                });
+            }
+        }
+
+        if (self.config.line_number) {
+            var lines = mem.split(u8, data, "\n");
+            var line_num: usize = 1;
+            while (lines.next()) |line| : (line_num += 1) {
+                const lnl = digitLen(line_num); // line number length
+                try self.writer.writeByteNTimes(' ', 4 - lnl);
+                const line_split_char = if (self.config.ascii_chars) "|" else "│";
+                try self.writer.print("{s}{d}{s} {s} ", .{ self.getColor(.Cyan), line_num, self.getColor(.Reset), self.setColor(line_split_char, .Gray) });
+                try self.writer.writeAll(line);
+                if (lines.index != null) try self.writer.writeByte('\n');
+            }
+        } else {
+            try self.writer.writeAll(data);
+        }
+
+        if (self.config.show_end) {
+            try self.writer.writeAll("<end>");
+        }
+    }
+};
---
+++ src/main.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+const Blo = @import("blo.zig").Blo;
+const process = std.process;
+const mem = std.mem;
+const fs = std.fs;
+const io = std.io;
+const log = std.log;
+const help_output =
+    \\Usage: blo [OPTION]... [FILE]...
+    \\With no FILE, read standard input.
+    \\
+    \\Options:
+    \\-n, --number          prints number of lines
+    \\-i, --info            prints the file info (size, mime, modification, etc)
+    \\-e, --show-end        prints <end> after file
+    \\-a, --ascii           uses ascii chars to print info and lines delimiter
+    \\-c, --no-color        disable printing colored output
+    \\-h, --help            display this help and exit
+    \\
+    \\Examples:
+    \\blo README.md         prints the README.md content
+    \\blo                   copy standard input to output
+;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = &arena.allocator;
+    const args = try process.argsAlloc(allocator);
+    var files = std.ArrayList([]u8).init(allocator);
+    defer {
+        files.deinit();
+        process.argsFree(allocator, args);
+        arena.deinit();
+    }
+
+    // zig fmt: off
+    var config = Blo.Config{
+        .ascii_chars = false,
+        .colors = true,
+        .show_end = false,
+        .line_number = false,
+        .info = false
+    };
+    // zig fmt: on
+
+    for (args[1..]) |arg| {
+        if (arg.len > 1 and arg[0] == '-') {
+            if (mem.eql(u8, arg, "-a") or mem.eql(u8, arg, "--ascii")) {
+                config.ascii_chars = true;
+            } else if (mem.eql(u8, arg, "-c") or mem.eql(u8, arg, "--no-color")) {
+                config.colors = false;
+            } else if (mem.eql(u8, arg, "-e") or mem.eql(u8, arg, "--show-end")) {
+                config.show_end = true;
+            } else if (mem.eql(u8, arg, "-n") or mem.eql(u8, arg, "--number")) {
+                config.line_number = true;
+            } else if (mem.eql(u8, arg, "-i") or mem.eql(u8, arg, "--info")) {
+                config.info = true;
+            } else if (mem.eql(u8, arg, "-h") or mem.eql(u8, arg, "--help")) {
+                log.info(help_output, .{});
+            } else {
+                log.err("unkown option {s}", .{arg});
+            }
+        } else {
+            try files.append(arg);
+        }
+    }
+
+    const blo = Blo.init(allocator, io.getStdOut(), config);
+
+    for (files.items) |file, index| {
+        blo.printFile(file) catch |err| {
+            if (err == error.FileTooBig) log.err("{s} is too big", .{file});
+            return err;
+        };
+        if (index < files.items.len - 1) {
+            try blo.write("\n\n");
+        }
+    }
+}
