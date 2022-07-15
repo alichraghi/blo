b24dcf64cc51b1a868294aa5608b0c7ab4d01d61
alichraghi <alichraghi@pm.me>
2022-07-15 20:33:57 +0430

    general improvements

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,19 @@
 const std = @import("std");
-const Color = @import("term.zig").Color;
+const term = @import("term.zig");
+const syntax = @import("syntax.zig");
 const process = std.process;
 const mem = std.mem;
 const fs = std.fs;
+const math = std.math;
 const stdout = std.io.getStdOut();
 const stdin = std.io.getStdIn();
-const syntax = @import("syntax.zig");
+const outWriter = stdout.writer();
+const inReader = stdin.reader();
 
+const max_file_size = math.pow(usize, 1024, 4) * 1; // 1TB
 const help_output =
     \\Usage: blo [OPTION]... [FILE]...
-    \\With no FILE, read standard input.
+    \\With no FILE, reads standard input.
     \\
     \\Options:
     \\-n, --number          prints number of lines
@@ -28,52 +32,24 @@ pub const Error = error{
     UnkownOption,
 };
 
+pub const Config = struct {
+    highlight: bool,
+    ascii_chars: bool,
+    info: bool,
+    colors: bool,
+    show_end: bool,
+    line_number: bool,
+};
+
 pub const Blo = struct {
-    const Self = Blo;
-    const max_file_size = 1024 * 1024 * 10;
-
-    pub const Config = struct {
-        highlight: bool,
-        ascii_chars: bool,
-        info: bool,
-        colors: bool,
-        show_end: bool,
-        line_number: bool,
-    };
-
     allocator: mem.Allocator,
-    out: fs.File,
-    writer: fs.File.Writer,
     config: Config,
 
-    pub fn init(allocator: mem.Allocator, out: fs.File, config: Config) Self {
-        var modif_config = config;
-
-        if (!out.supportsAnsiEscapeCodes()) {
-            modif_config.ascii_chars = true;
-            modif_config.colors = false;
-        }
-
-        return .{ .allocator = allocator, .out = out, .writer = out.writer(), .config = modif_config };
-    }
-
-    pub fn write(self: Self, data: []const u8) !void {
-        try self.writer.writeAll(data);
-    }
-
-    pub fn writeNTime(self: Self, data: []const u8) !void {
-        try self.writer.writeByteNTimes(data);
-    }
-
-    fn writeSliceNTime(self: Self, slice: []const u8, n: usize) !void {
-        var i = n;
-        while (i > 0) : (i -= 1) {
-            try self.writer.writeAll(slice);
-        }
-    }
-
-    fn print(self: Self, comptime format: []const u8, args: anytype) !void {
-        try self.writer.print(format, args);
+    pub fn init(allocator: mem.Allocator, config: Config) Blo {
+        return .{
+            .allocator = allocator,
+            .config = config,
+        };
     }
 
     // count number length
@@ -82,16 +58,22 @@ pub const Blo = struct {
         return 1 + digitLen(n / 10);
     }
 
-    // Format to human readable size like 10B, 10KB, etc
-    fn fmtSize(self: Self, bytes: usize) ![]const u8 {
-        const suffix = &[_][]const u8{ "B", "KB", "MB" };
-        var size = bytes;
-        var i: usize = 0;
-        while (size >= 1024) {
-            size /= 1024;
-            i += 1;
-        }
-        return std.fmt.allocPrint(self.allocator, "{d} {s}", .{ size, suffix[i] }) catch "Unkown";
+    fn withColor(self: Blo, value: []const u8, color: term.Color, out_color: ?term.Color) ![]const u8 {
+        if (self.config.colors) {
+            var res = std.ArrayList(u8).init(self.allocator);
+            defer res.deinit();
+
+            try res.appendSlice(color.toCode());
+            try res.appendSlice(value);
+            if (out_color) |out|
+                try res.appendSlice(out.toCode());
+
+            return res.toOwnedSlice();
+        } else return value;
+    }
+
+    pub fn echoStdin(self: Blo) void {
+        _ = self;
     }
 
     fn fillSlice(slice: []u8, value: []const u8) void {
@@ -101,59 +83,61 @@ pub const Blo = struct {
         }
     }
 
-    fn setColor(self: Self, value: []const u8, color: Color, out: ?Color) ![]const u8 {
-        if (self.config.colors) {
-            var res = std.ArrayList(u8).init(self.allocator);
-            defer res.deinit();
-            try res.appendSlice(color.toCode());
-            try res.appendSlice(value);
-            if (out) |out_color| {
-                try res.appendSlice(out_color.toCode());
-            } else {
-                try res.appendSlice(Color.Reset.toCode());
-            }
-            return res.toOwnedSlice();
-        } else return value;
-    }
-
-    pub fn printFile(self: Self, path: []const u8) !void {
+    pub fn printFile(self: Blo, path: []const u8) !void {
         // reading file
         const file = try fs.cwd().openFile(path, .{});
-        const data = try file.readToEndAlloc(self.allocator, max_file_size);
+        // const data = try file.readToEndAlloc(self.allocator, max_file_size);
 
-        // writing file information
+        // writing file information (header)
         if (self.config.info) {
             // file stat
             const stat = try file.stat();
+            // size
+            var size_buf_stream = std.io.fixedBufferStream(@as(*[6]u8, undefined));
+            try std.fmt.fmtIntSizeBin(stat.size).format("", .{}, size_buf_stream.writer());
+            const size = size_buf_stream.getWritten();
 
-            // file size
-            const size = try self.fmtSize(@intCast(usize, stat.size));
+            // space that caused by line numbers
+            const space = if (self.config.line_number)
+                " " ** 5
+            else
+                "";
 
-            // file info
-            const stat_len = 2; // path - size - owner
-            const total_stat_len = path.len + size.len;
-            const space = if (self.config.line_number) " " ** 5 else "";
-
-            // printing
             if (self.config.ascii_chars) {
-                const width_len = (total_stat_len + (stat_len * 3) + 5);
-                const width = try self.allocator.alloc(u8, width_len);
-                defer self.allocator.free(width);
-                for (width) |*char| char.* = '-';
+                // ASCII Header
+                const header_width = "--  --  --".len + path.len + size.len;
 
-                try self.print(
+                // Horizontal border
+                const horiz_border = try self.allocator.alloc(u8, header_width);
+                defer self.allocator.free(horiz_border);
+                for (horiz_border) |*c| c.* = '-';
+
+                // print
+                try outWriter.print(
                     \\{s}{s}{s}
                     \\{s}-- {s} -- {s} --
-                    \\{s}{s}{s}
+                    \\{s}{s}
                     \\
-                , .{ space, Color.Gray.toCode(), width, space, self.setColor(path, .Yellow, .Gray), self.setColor(size, .Magenta, .Gray), space, width, Color.Reset.toCode() });
+                , .{
+                    space,
+                    term.Color.gray.toCode(),
+                    horiz_border,
+
+                    space,
+                    self.withColor(path, .bright_magenta, .gray),
+                    self.withColor(size, .green, .gray),
+
+                    space,
+                    horiz_border,
+                });
             } else {
-                const side_margin = 2;
-                const brick = "─";
-                var path_width = try self.allocator.alloc(u8, (path.len + side_margin) * brick.len);
-                var size_width = try self.allocator.alloc(u8, (size.len + side_margin) * brick.len);
-                fillSlice(path_width, brick);
-                fillSlice(size_width, brick);
+                const padding_width = 2;
+                const border_char = "─";
+
+                var path_width = try self.allocator.alloc(u8, (path.len + padding_width) * border_char.len);
+                var size_width = try self.allocator.alloc(u8, (size.len + padding_width) * border_char.len);
+                fillSlice(path_width, border_char);
+                fillSlice(size_width, border_char);
                 defer {
                     self.allocator.free(path_width);
                     self.allocator.free(size_width);
@@ -161,78 +145,77 @@ pub const Blo = struct {
 
                 const left_bottom_corner = if (self.config.line_number) "├" else "└";
 
-                try self.print(
+                try outWriter.print(
                     \\{s}{s}┌{s}┬{s}┐
                     \\{s}│ {s} │ {s} │
-                    \\{s}{s}{s}┴{s}┘{s}
+                    \\{s}{s}{s}┴{s}┘
                     \\
                 , .{
                     space,
-                    Color.Gray.toCode(),
+                    term.Color.gray.toCode(),
                     path_width,
                     size_width,
                     space,
-                    self.setColor(path, .Yellow, .Gray),
-                    self.setColor(size, .Magenta, .Gray),
+                    self.withColor(path, .bright_magenta, .gray),
+                    self.withColor(size, .green, .gray),
                     space,
                     left_bottom_corner,
                     path_width,
                     size_width,
-                    Color.Reset.toCode(),
                 });
             }
         }
 
         // check for line numbers
-        if (self.config.line_number) {
-            var line_num: usize = 1;
-            if (self.config.highlight) {
-                var syntax_iterator = syntax.SyntaxIterator.init(.json, null, data);
-                const line_split_char = if (self.config.ascii_chars) "|" else "│";
-                var i: usize = 0;
-                while (syntax_iterator.next()) |token| : (i += 1) {
-                    const lnl = digitLen(line_num); // line number length
-                    if (i == 0) {
-                        try self.writer.writeByteNTimes(' ', 4 - lnl);
-                        try self.writer.print("{s}{d}{s} {s} ", .{ Color.Cyan.toCode(), line_num, Color.Reset.toCode(), self.setColor(line_split_char, .Gray, null) });
-                        try self.writer.writeAll(token.color.toCode());
-                        try self.writer.writeAll(data[token.start..token.end]);
-                    } else if (data[token.start..token.end][0] == '\n') {
-                        line_num += 1;
-                        try self.writer.writeByte('\n');
-                        try self.writer.writeByteNTimes(' ', 4 - lnl);
-                        try self.writer.print("{s}{d}{s} {s} ", .{ Color.Cyan.toCode(), line_num, Color.Reset.toCode(), self.setColor(line_split_char, .Gray, null) });
-                    } else {
-                        try self.writer.writeAll(token.color.toCode());
-                        try self.writer.writeAll(data[token.start..token.end]);
-                    }
-                }
-            } else {
-                var lines = mem.split(u8, data, "\n");
-                const line_split_char = if (self.config.ascii_chars) "|" else "│";
-                while (lines.next()) |line| : (line_num += 1) {
-                    const lnl = digitLen(line_num); // line number length
-                    try self.writer.writeByteNTimes(' ', 4 - lnl);
-                    try self.writer.print("{s}{d}{s} {s} ", .{ Color.Cyan.toCode(), line_num, Color.Reset.toCode(), self.setColor(line_split_char, .Gray, null) });
-                    try self.writer.writeAll(line);
-                    if (lines.index != null) try self.writer.writeByte('\n');
-                }
-            }
-        } else {
-            if (self.config.highlight) {
-                var syntax_iterator = syntax.SyntaxIterator.init(.json, null, data);
-                while (syntax_iterator.next()) |token| {
-                    try self.writer.writeAll(token.color.toCode());
-                    try self.writer.writeAll(data[token.start..token.end]);
-                }
-            } else {
-                try self.writer.writeAll(data);
-            }
-        }
+        // if (self.config.line_number) {
+        //     var line_num: usize = 1;
+        //     if (self.config.highlight) {
+        //         var syntax_iterator = syntax.SyntaxIterator.init(.json, null, data);
+        //         const line_split_char = if (self.config.ascii_chars) "|" else "│";
+        //         var i: usize = 0;
+        //         while (syntax_iterator.next()) |token| : (i += 1) {
+        //             const lnl = digitLen(line_num); // line number length
+        //             if (i == 0) {
+        //                 try outWriter.writeByteNTimes(' ', 4 - lnl);
+        //                 try outWriter.print("{s}{d}{s} {s} ", .{ Color.Cyan.toCode(), line_num, Color.Reset.toCode(), self.withColor(line_split_char, .Gray, null) });
+        //                 try outWriter.writeAll(token.color.toCode());
+        //                 try outWriter.writeAll(data[token.start..token.end]);
+        //             } else if (data[token.start..token.end][0] == '\n') {
+        //                 line_num += 1;
+        //                 try outWriter.writeByte('\n');
+        //                 try outWriter.writeByteNTimes(' ', 4 - lnl);
+        //                 try outWriter.print("{s}{d}{s} {s} ", .{ Color.Cyan.toCode(), line_num, Color.Reset.toCode(), self.withColor(line_split_char, .Gray, null) });
+        //             } else {
+        //                 try outWriter.writeAll(token.color.toCode());
+        //                 try outWriter.writeAll(data[token.start..token.end]);
+        //             }
+        //         }
+        //     } else {
+        //         var lines = mem.split(u8, data, "\n");
+        //         const line_split_char = if (self.config.ascii_chars) "|" else "│";
+        //         while (lines.next()) |line| : (line_num += 1) {
+        //             const lnl = digitLen(line_num); // line number length
+        //             try outWriter.writeByteNTimes(' ', 4 - lnl);
+        //             try outWriter.print("{s}{d}{s} {s} ", .{ Color.Cyan.toCode(), line_num, Color.Reset.toCode(), self.setColor(line_split_char, .Gray, null) });
+        //             try outWriter.writeAll(line);
+        //             if (lines.index != null) try outWriter.writeByte('\n');
+        //         }
+        //     }
+        // } else {
+        //     if (self.config.highlight) {
+        //         var syntax_iterator = syntax.SyntaxIterator.init(.json, null, data);
+        //         while (syntax_iterator.next()) |token| {
+        //             try outWriter.writeAll(token.color.toCode());
+        //             try outWriter.writeAll(data[token.start..token.end]);
+        //         }
+        //     } else {
+        //         try outWriter.writeAll(data);
+        //     }
+        // }
 
         // show file end with <end>
         if (self.config.show_end) {
-            try self.writer.writeAll("<end>");
+            try outWriter.writeAll("<end>");
         }
     }
 };
@@ -248,7 +231,7 @@ pub fn main() !void {
         arena.deinit();
     }
 
-    var config = Blo.Config{
+    var config = Config{
         .highlight = true,
         .ascii_chars = false,
         .colors = true,
@@ -283,17 +266,21 @@ pub fn main() !void {
     if (files.items.len == 0) {
         while (true) {
             var buf: [1024]u8 = undefined;
-            if (try stdin.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
-                try stdout.writer().print("{s}\n", .{line});
+            if (try inReader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+                try outWriter.print("{s}\n", .{line});
             } else break;
         }
     } else {
-        const blo = Blo.init(allocator, stdout, config);
+        if (!stdout.supportsAnsiEscapeCodes()) {
+            config.ascii_chars = true;
+            config.colors = false;
+        }
+        const blo = Blo.init(allocator, config);
 
         for (files.items) |file, index| {
             try blo.printFile(file);
             if (index < files.items.len - 1) {
-                try blo.write("\n\n");
+                try stdout.writeAll("\n\n");
             }
         }
     }
--- src/term.zig
+++ src/term.zig
@@ -1,41 +1,78 @@
-pub const Color = enum {
-    Reset,
-    Black,
-    Red,
-    Green,
-    Yellow,
-    Blue,
-    Magenta,
-    Cyan,
-    White,
-    Gray,
-    BrightRed,
-    BrightGreen,
-    BrightYellow,
-    BrightBlue,
-    BrightMagenta,
-    BrightCyan,
-    BrightWhite,
+/// Tab
+pub const TAB = "\x09";
+/// Line Feed
+pub const LF = "\x0A";
+/// From Feed
+pub const FF = "\x0C";
+/// Carriage Return
+pub const CR = "\x0D";
+/// ANSI Escape Code
+pub const ESC = "\x1B";
+
+pub const Sgr = enum(u4) {
+    reset = 0,
+    bold = 1,
+    dim = 2,
+    italic = 3,
+    underline = 4,
+    blinking = 5,
+    reverse = 7,
+    hidden = 8,
+    strike = 9,
+
+    pub fn toCode(self: Sgr) []const u8 {
+        return switch (self) {
+            .reset => ESC ++ "[0m",
+            .bold => ESC ++ "[1m",
+            .dim => ESC ++ "[2m",
+            .italic => ESC ++ "[3m",
+            .underline => ESC ++ "[4m",
+            .blinking => ESC ++ "[5m",
+            .reverse => ESC ++ "[7m",
+            .hidden => ESC ++ "[8m",
+            .strike => ESC ++ "[9m",
+        };
+    }
+};
+
+pub const Color = enum(u7) {
+    black = 30,
+    red = 31,
+    green = 32,
+    yellow = 33,
+    blue = 34,
+    magenta = 35,
+    cyan = 36,
+    white = 37,
+    default = 39,
+    gray = 90,
+    bright_red = 91,
+    bright_green = 92,
+    bright_yellow = 93,
+    bright_blue = 94,
+    bright_magenta = 95,
+    bright_cyan = 96,
+    bright_white = 97,
 
     pub fn toCode(self: Color) []const u8 {
         return switch (self) {
-            .Reset => "\x1b[0m",
-            .Black => "\x1b[30m",
-            .Red => "\x1b[31m",
-            .Green => "\x1b[32m",
-            .Yellow => "\x1b[33m",
-            .Blue => "\x1b[34m",
-            .Magenta => "\x1b[35m",
-            .Cyan => "\x1b[36m",
-            .White => "\x1b[37m",
-            .Gray => "\x1b[90m",
-            .BrightRed => "\x1b[91m",
-            .BrightGreen => "\x1b[92m",
-            .BrightYellow => "\x1b[93m",
-            .BrightBlue => "\x1b[94m",
-            .BrightMagenta => "\x1b[95m",
-            .BrightCyan => "\x1b[96m",
-            .BrightWhite => "\x1b[97m",
+            .black => ESC ++ "[30m",
+            .red => ESC ++ "[31m",
+            .green => ESC ++ "[32m",
+            .yellow => ESC ++ "[33m",
+            .blue => ESC ++ "[34m",
+            .magenta => ESC ++ "[35m",
+            .cyan => ESC ++ "[36m",
+            .white => ESC ++ "[37m",
+            .default => ESC ++ "[39m",
+            .gray => ESC ++ "[90m",
+            .bright_red => ESC ++ "[91m",
+            .bright_green => ESC ++ "[92m",
+            .bright_yellow => ESC ++ "[93m",
+            .bright_blue => ESC ++ "[94m",
+            .bright_magenta => ESC ++ "[95m",
+            .bright_cyan => ESC ++ "[96m",
+            .bright_white => ESC ++ "[97m",
         };
     }
 };
